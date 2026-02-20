d7718e2b376bee3027f37955daf3a9af70d14d5d
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-20 14:58:54 +0600

    11

---
+++ 800 Rating/Selling_Coins.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/SELLCOIN?tab=ide
+int main(){
+    int a,b ; cin>>a>>b;
+    cout<<a+(b*2);
+    return 0;
+}
