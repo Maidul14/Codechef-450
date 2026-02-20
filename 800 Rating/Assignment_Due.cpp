5eafeed7dc8c6cf9574119f3b7a69a60e3492d33
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-20 22:10:01 +0600

    36

---
+++ 800 Rating/Assignment_Due.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link:https://www.codechef.com/problems/P1_175?tab=ide
+int main(){
+    int a,b; cin>>a>>b;
+    if(a<=b){
+        cout<<"YES";
+    }  
+    else cout<<"NO";
+    return 0;
+}
