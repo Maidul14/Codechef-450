4e776b6973028c310e6bda4161dcd5d601a77c1d
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-20 14:36:37 +0600

    7

---
+++ Selling_Sandwiches.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/SANDWSHOP?tab=ide
+int main(){
+    int x,y,z; cin>>x>>y>>z;
+    int result = x-(y+z);
+    cout<<result;  
+    return 0;
+}
