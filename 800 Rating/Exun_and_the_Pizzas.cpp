450a8b251fd6e6d57e5e9b8e7d3e0366a0bc428e
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-20 15:03:34 +0600

    13

---
+++ 800 Rating/Exun_and_the_Pizzas.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link :https://www.codechef.com/problems/EXUNPIZZA?tab=ide
+int main() {
+	int a,b,c ; cin>>a>>b>>c;
+	int d = a-b ;
+	cout<<d*c;
+
+}
