12424a4887b5980752c5af55852687dc2ae0eb41
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-20 15:21:21 +0600

    20

---
+++ 800 Rating/Bitcoin_Market.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link :https://www.codechef.com/problems/P1209
+int main(){
+    int n ; cin>>n;
+    if(4>=n){
+        cout<<"YES";
+    }  
+    else{
+        cout<<"NO";
+    }
+    return 0;
+}
