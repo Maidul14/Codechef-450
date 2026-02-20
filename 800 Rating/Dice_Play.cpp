211d1a25d05203a46decb480ea7396adc6c76638
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-20 15:14:12 +0600

    17

---
+++ 800 Rating/Dice_Play.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link :https://www.codechef.com/problems/P1HOME
+int main(){
+    int a,b; cin>>a>>b;
+    if(a==b){
+        cout<<"Yes";
+    }  
+    else{
+        cout<<"No";
+    }
+    return 0;
+}
