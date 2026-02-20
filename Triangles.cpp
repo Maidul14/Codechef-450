5e3ebabf1bfd5f183fba9caa09ce5ac1d22c621b
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-20 14:34:48 +0600

    6

---
+++ Triangles.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link :https://www.codechef.com/problems/TRIANGLE7
+int main(){
+    int x,y; cin>>x>>y;
+    int res = 180-(x+y);
+    cout<<res;  
+    return 0;
+}
