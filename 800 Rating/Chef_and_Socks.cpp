d07ff79d9d9a41e64cc23fcfb3b3c15825c21699
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-20 22:16:53 +0600

    43

---
+++ 800 Rating/Chef_and_Socks.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link :https://www.codechef.com/problems/CHEFSOCKS?tab=ide
+int main(){
+    int a,x,y ; cin>>a>>x>>y;
+    if((x+y)>=a){
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+    return 0;
+}
