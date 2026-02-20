fd208a2a9e6f23e9932d551e0f090f60510e53dd
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-20 14:33:38 +0600

    5

---
+++ Remaining_Money.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link :https://www.codechef.com/problems/REMMON
+int main() {
+    int a,b,c ; cin>>a>>b>>c;
+    int rem = b*c;
+    int res = a-rem;
+    cout<<res;
+
+}
