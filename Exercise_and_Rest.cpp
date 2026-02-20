2280e22285cf59a207822cd5458a585c3619cb11
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-20 14:29:15 +0600

    3

---
+++ Exercise_and_Rest.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/EXREST
+int main(){
+    int n ; cin>>n;
+    int res = n*3;
+    cout<<res;  
+    return 0;
+}
