ff29b44ac3048e484b90d30e6f1dfbc081aeb8f0
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-20 14:26:27 +0600

    2

---
+++ Gambling.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link :https://www.codechef.com/problems/GAMBLING
+int main(){
+    int n ; cin>>n;
+    int res = 7-n;
+    cout<<res<<endl;  
+    return 0;
+}
