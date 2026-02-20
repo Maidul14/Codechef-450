b58be76fe11eb9b3523f08cc39918c178b287df1
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-20 21:34:07 +0600

    27

---
+++ 800 Rating/Diwali_Discount.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/DIWALIDISC?tab=ide
+int main(){
+      int A,B; cin>>A>>B;
+      if(A>B){
+        cout<<A-B;
+      }
+      else{
+        cout<<0;
+      }
+    return 0;
+}
