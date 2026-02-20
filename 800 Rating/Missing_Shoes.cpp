672b9f3ffff27ea4ad699b084c88ebb400f2efb0
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-20 15:00:57 +0600

    12

---
+++ 800 Rating/Missing_Shoes.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link :https://www.codechef.com/problems/SHOM?tab=ide
+int main(){
+    int a,b ; cin>>a>>b;
+    int c = abs(a-b);
+    cout<<c;
+    return 0;
+}
