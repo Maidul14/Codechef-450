e5929752e1fbbfe98dd7aafb2f3e9ea85b58c163
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-20 14:53:46 +0600

    10

---
+++ Spring_Cleaning.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link :https://www.codechef.com/problems/SPRCLN
+int main(){
+    int small, big ; cin>>small>>big;
+    int time1= small*30;
+    int time2= big*60;
+    cout<<time1+time2;  
+    return 0;
+}
