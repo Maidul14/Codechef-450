913952933d9e74bb34d237bfb0102535c70f63f9
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-20 22:11:33 +0600

    38

---
+++ 800 Rating/Protein_Diet.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link: https://www.codechef.com/problems/LMP1
+int main(){
+    int x, y ; cin>>x>>y;
+    if(x>=y){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
