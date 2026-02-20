5039439a1fbff0880a32b0faf689545220a69c45
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-20 22:07:55 +0600

    32

---
+++ 800 Rating/Basketball_Score.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link :https://www.codechef.com/problems/P1BAR
+int main(){
+    int x,y; cin>>x>>y;
+    int res = x*3+ y*2;
+    cout<<res;
+    return 0;
+}
