d5b3393ce0367919f4543687209b63405d53fba4
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-20 22:19:19 +0600

    48

---
+++ 800 Rating/Competition_of_4.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link :https://www.codechef.com/problems/COMP4
+int main(){
+    int n ; cin>>n;
+    switch (n)
+    {
+    case 1:
+        cout<<8000;
+        break;
+    case 2:
+        cout<<4000;
+        break;
+    case 3:
+        cout<<2000;
+        break;
+    case 4:
+        cout<<1000;
+    default:
+        break;
+    }  
+    return 0;
+}
