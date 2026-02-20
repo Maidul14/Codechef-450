41003da742ae9419845c083c8eeb9fdb81291d72
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-20 22:04:40 +0600

    30

---
+++ 800 Rating/Happy_New_Year.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link:https://www.codechef.com/problems/NEWYEAR
+int main(){
+    int n ; cin>>n;
+    int res = 24-n;
+    cout<<res;  
+    return 0;
+}
