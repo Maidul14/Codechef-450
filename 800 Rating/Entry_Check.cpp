4679f0ba0d69afd6e18c86a9a0575277df078cdd
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-20 15:42:27 +0600

    22

---
+++ 800 Rating/Entry_Check.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link :https://www.codechef.com/problems/P1169
+int main(){
+    int age ; cin>>age ; 
+    if(age>=10){
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+    return 0;
+}
