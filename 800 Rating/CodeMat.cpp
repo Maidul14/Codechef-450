83e4288037d81811cd2e6ad5d06d81bb7cc95396
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-20 15:16:58 +0600

    19

---
+++ 800 Rating/CodeMat.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link :https://www.codechef.com/problems/CDMT?tab=ide
+int main(){
+    int a,b; cin>>a>>b;
+    if(a<b){
+        cout<<"Yes";
+    }
+    else{
+        cout<<"No";
+    }
+    return 0;
+}
