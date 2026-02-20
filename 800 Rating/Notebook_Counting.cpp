07e72fca628de92550501351c0dc809eb886bcfe
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-20 15:15:33 +0600

    18

---
+++ 800 Rating/Notebook_Counting.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link :https://www.codechef.com/problems/NOTECNT
+int main() {
+	int a,b; cin>>a>>b;
+	int page = (b*50)*2;
+    int result = page*a;
+    cout<<result;
+}
