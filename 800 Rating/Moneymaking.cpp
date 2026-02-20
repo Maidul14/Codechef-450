5dec2d6739b049ad68789780c639ef6930b7785b
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-20 15:09:46 +0600

    15

---
+++ 800 Rating/Moneymaking.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link :https://www.codechef.com/problems/NUGGET
+int main(){
+    int x,y ; cin>>x>>y;
+    int low = x*5000;
+    int high = y*9800;
+    cout<<high+low;  
+    return 0;
+}
