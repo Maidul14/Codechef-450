8e39322f7ec05b5509d78277f9882c6a627c2728
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-20 22:12:32 +0600

    39

---
+++ 800 Rating/Squid_Game_Piggy_Bank.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link :https://www.codechef.com/problems/SQUIDBANK
+int main() {
+    int n,k; cin>>n>>k;
+    int rem = n-k;
+    int res = rem*10000;
+    cout<<res;
+}
