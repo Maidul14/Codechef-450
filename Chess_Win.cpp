4313749e5c0302db2b6cf60948b316efae6605c8
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-20 22:05:21 +0600

    31

---
+++ Chess_Win.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link:https://www.codechef.com/problems/CHESSWIN
+int main(){
+    int a,b; cin>>a>>b;
+    cout<<(b-a+1);
+     
+    return 0;
+}
