353a20ba822de8f7dbe7c07844b16efca65529f2
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-20 14:31:34 +0600

    4

---
+++ Time_Penalty.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link :https://www.codechef.com/problems/WAPEN
+int main(){
+    int x,y; cin>>x>>y;
+    int result = x+(y*10);
+    cout<<result;  
+    return 0;
+}
