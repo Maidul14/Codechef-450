7b198ca7c7023e9ba2fc8dd4ac9fc688238cad50
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-25 02:39:47 +0600

    900

---
+++ 900 Rating/Cashback.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link:https://www.codechef.com/problems/CASHBACK
+int main() {
+	int n ; cin>>n ;
+	if(n>=200){
+	    cout<<n-50;
+	}
+	else{
+	    cout<<n;
+	}
+
+}
