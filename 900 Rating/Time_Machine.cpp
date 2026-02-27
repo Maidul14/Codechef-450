eb186269a195f5bfb22eac5b423a2d63baa33acd
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-28 00:30:14 +0600

    900

---
+++ 900 Rating/Time_Machine.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem linkk : https://www.codechef.com/problems/TIMA?tab=ide
+int main(){
+    int n ; cin>>n ;
+    if(n>=2050 || n+25>=2050){
+        cout<<"Yes"<<endl;
+    }
+    else{
+        cout<<"No"<<endl;
+    }
+      
+    return 0;
+}
