21a380414788c89baf091a293833a96f521b5e74
maidul14 <md.maidulislammahin14@gmail.com>
2026-03-02 01:26:24 +0600

    900

---
+++ 900 Rating/Cab_Rides.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/CABRIDE?tab=ide
+int main(){
+    int  test ; cin>>test;
+    while (test--)
+    {
+        int n ; cin>>n ;
+        int mx = max(200, n*100);
+        cout<<mx<<endl;
+    }
+      
+    return 0;
+}
