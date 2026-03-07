c60591d823fc1cbff015ab7485931b16ce225c0d
maidul14 <md.maidulislammahin14@gmail.com>
2026-03-07 19:34:25 +0600

    900

---
+++ 900 Rating/Access_Code_Equality.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/WECNITK
+int main() {
+	string s ; cin>>s;
+	if(s=="WECNITK"){
+	    cout<<"Welcome to Web Club!";
+	}
+	else{
+	    cout<<"Access denied";
+	}
+
+}
