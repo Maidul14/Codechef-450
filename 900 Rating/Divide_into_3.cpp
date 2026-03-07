50f541e639b9348e574fef38c35157b83cc530ad
maidul14 <md.maidulislammahin14@gmail.com>
2026-03-07 19:24:06 +0600

    900

--- 900 Rating/Divide_into_3.cpp
+++ 900 Rating/Divide_into_3.cpp
@@ -1,19 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+//problem link : https://www.codechef.com/problems/DIVIDE3
 int main(){
-    int arr[5];
-    int target = 22;
-    for(int i = 0 ; i<5 ; i++){
-        cin>>arr[i];
-    }  
-    for (int  i = 0; i < 5; i++)
-    {
-       if(arr[i]== target){
-        cout<<"Found" ;
-        break;
-       }
-    }
-   
+    int n ; cin>>n ;
+   if(n%3==0){
+    cout<<0;
+   }
+   else {
+    cout<<1;
+   }
     return 0;
 }
