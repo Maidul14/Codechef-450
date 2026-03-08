c96a1442345c10cad10e7f0f03023e45667d9963
maidul14 <md.maidulislammahin14@gmail.com>
2026-03-08 14:03:37 +0600

    900

---
+++ Number_Reduction.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/RED23?tab=ide
+int main(){
+    int test ; cin>>test;
+    while (test--)
+    {
+        int n ; cin>>n;
+        if(n%3==0) cout<<3<<endl;
+        else cout<<1<<endl;
+        
+    }
+      
+    return 0;
+}
