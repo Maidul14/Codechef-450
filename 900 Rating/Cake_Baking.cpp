95775a5cdbbcbcaff2e72b48b6bc107059b14831
maidul14 <md.maidulislammahin14@gmail.com>
2026-03-08 13:21:59 +0600

    900

---
+++ 900 Rating/Cake_Baking.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/CAKEBAKE7?tab=ide
+int main(){
+    int n , m ; cin>>n>>m;
+    cout<<min(n, m-n);  
+    return 0;
+}
